f8e9ed5a6ccfb2ad8cc92b1c425fba351fc3b2af
Sekumpulan TxT <116649984+sekumpulan@users.noreply.github.com>
2022-11-27 23:37:48 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/sekumpulan/RTXZY-MD-BotWA/pulls
 `
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
